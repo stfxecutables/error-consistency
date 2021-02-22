f5414e96c298778c286568f7d20317159df4470f
Derek M Berger <dmberger.dev@gmail.com>
2021-02-22 12:23:03 -0500

    choose strong args for classifiers

---
+++ analysis/constants.py
@@ -0,0 +1,95 @@
+from pandas import DataFrame
+import os
+from pathlib import Path
+import numpy as np
+from typing import Any, Callable, Dict, List, Optional, Tuple, Type, Union
+from typing import cast, no_type_check
+from typing_extensions import Literal
+from itertools import repeat
+from tests.loading import load_diabetes, load_park, load_SPECT, load_trans
+from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression as LR
+from sklearn.ensemble import RandomForestClassifier as RF
+from sklearn.ensemble import AdaBoostClassifier as AdaBoost
+from sklearn.neural_network import MLPClassifier
+
+OUTDIR = Path(__file__).resolve().parent / "results"
+PLOT_OUTDIR = OUTDIR / "plots"
+RESULTS_DIR = OUTDIR / "dfs"
+
+for dir in [OUTDIR, PLOT_OUTDIR, RESULTS_DIR]:
+    if not dir.exists():
+        os.makedirs(dir)
+
+REPS_PER_PERCENT = 50
+KFOLD_REPS = 100
+REPS_PER_PERCENT = 5
+KFOLD_REPS = 10
+
+PERCENTS = np.linspace(0, 1, 21)[1:-1]  # 5, 10, ..., 95
+COLS = [f"{e:1.0f}" for e in PERCENTS * 100]
+N_ROWS = len(COLS) * REPS_PER_PERCENT
+
+DATASET_NAMES = ["Diabetes", "Transfusion", "Parkinsons", "SPECT"]
+KNN1_ARGS = {name: dict(n_neighbors=1) for name in DATASET_NAMES}
+KNN3_ARGS = {name: dict(n_neighbors=3) for name in DATASET_NAMES}
+KNN5_ARGS = {name: dict(n_neighbors=5) for name in DATASET_NAMES}
+KNN10_ARGS = {name: dict(n_neighbors=10) for name in DATASET_NAMES}
+LR_ARGS = {
+    "Diabetes": dict(solver="liblinear", penalty="l1", C=1.7, max_iter=250),
+    "Transfusion": dict(solver="liblinear", penalty="l1", C=1.6, max_iter=500),
+    "Parkinsons": dict(solver="liblinear", penalty="l2", C=0.7, max_iter=500),
+    "SPECT": dict(solver="liblinear", penalty="l1", C=0.4, max_iter=500),
+}
+SVC_ARGS: Dict = {  # basically doesn't matter, all perform about the same
+    "Diabetes": dict(kernel="rbf", C=1.0),
+    "Transfusion": dict(kernel="rbf", C=5.0),
+    "Parkinsons": dict(kernel="rbf", C=52.0),
+    "SPECT": dict(kernel="rbf", C=50.0),
+}
+RF_ARGS: Dict = {
+    "Diabetes": dict(n_estimators=400, min_samples_leaf=4, max_features=0.25, max_depth=20),
+    "Transfusion": dict(n_estimators=20, min_samples_leaf=1, max_features=0.25, max_depth=2),
+    "Parkinsons": dict(n_estimators=20, min_samples_leaf=2, max_features="auto", max_depth=2),
+    "SPECT": dict(n_estimators=20, min_samples_leaf=3, max_features=0.5, max_depth=8),
+}
+ADA_ARGS: Dict = {
+    "Diabetes": dict(n_estimators=50, learning_rate=0.52),
+    "Transfusion": dict(n_estimators=100, learning_rate=0.0436),
+    "Parkinsons": dict(n_estimators=200, learning_rate=0.00452),
+    "SPECT": dict(n_estimators=200, learning_rate=0.0285),
+}
+MLP_ARGS: Dict = {  # all found via random search with ray.tune
+    "Diabetes": dict(
+        hidden_layer_sizes=tuple(repeat(4, times=6)), alpha=0.0002, batch_size=32, max_iter=750
+    ),
+    "Transfusion": dict(
+        hidden_layer_sizes=tuple(repeat(4, times=6)), alpha=5e-6, batch_size=32, max_iter=750
+    ),
+    "Parkinsons": dict(
+        hidden_layer_sizes=tuple(repeat(4, times=6)), alpha=2e-5, batch_size=32, max_iter=1000
+    ),
+    "SPECT": dict(
+        hidden_layer_sizes=tuple(repeat(4, times=6)), alpha=0.0005, batch_size=32, max_iter=500
+    ),
+}
+
+
+CLASSIFIERS: Dict[str, Tuple[Type, Dict]] = {
+    "KNN-1": (KNN, KNN1_ARGS),
+    "KNN-3": (KNN, KNN3_ARGS),
+    "KNN-5": (KNN, KNN5_ARGS),
+    "KNN-10": (KNN, KNN10_ARGS),
+    "Logistic Regression": (LR, LR_ARGS),
+    "SVM Classifier": (SVC, SVC_ARGS),
+    "Random Forest": (RF, RF_ARGS),
+    "AdaBoosted DTree": (AdaBoost, ADA_ARGS),
+    "MLP": (MLPClassifier, MLP_ARGS),
+}
+DATA: Dict[str, Tuple[DataFrame, DataFrame]] = {
+    "Diabetes": load_diabetes(),
+    "Parkinsons": load_park(),
+    "Transfusion": load_trans(),
+    "SPECT": load_SPECT(),
+}
---
+++ analysis/downsampling.py
@@ -0,0 +1,291 @@
+import os
+import sys
+from pathlib import Path
+from tqdm import tqdm
+from typing import Any, Callable, Dict, List, Optional, Tuple, Type, Union
+from typing import cast, no_type_check
+from typing_extensions import Literal
+from itertools import repeat
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pytest
+import seaborn as sbn
+from argparse import ArgumentParser, Namespace
+from numpy import ndarray
+from pandas import DataFrame, Series
+from enum import Enum
+from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression as LR
+from sklearn.ensemble import RandomForestClassifier as RF
+from sklearn.ensemble import AdaBoostClassifier as AdaBoost
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from warnings import filterwarnings
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from analysis.constants import (
+    PLOT_OUTDIR,
+    OUTDIR,
+    RESULTS_DIR,
+    N_ROWS,
+    REPS_PER_PERCENT,
+    KFOLD_REPS,
+    PERCENTS,
+    COLS,
+    DATASET_NAMES,
+    KNN1_ARGS,
+    KNN3_ARGS,
+    KNN5_ARGS,
+    KNN10_ARGS,
+    LR_ARGS,
+    SVC_ARGS,
+    RF_ARGS,
+    ADA_ARGS,
+    MLP_ARGS,
+    CLASSIFIERS,
+    DATA,
+)
+from error_consistency.consistency import (
+    ErrorConsistencyKFoldHoldout,
+    ErrorConsistencyKFoldInternal,
+)
+
+
+def argparse_setup() -> ArgumentParser:
+    parser = ArgumentParser(description="Plot effect of downsampling on error consistency.")
+    parser.add_argument(
+        "--plot-dir", type=Path, help="directory to save generated plots", default=PLOT_OUTDIR
+    )
+    parser.add_argument(
+        "--results-dir",
+        type=Path,
+        help="directory to save generated data for plots",
+        default=RESULTS_DIR,
+    )
+    parser.add_argument(
+        "--kfold-reps", type=int, help="times to repeat k-fold per downsampling", default=KFOLD_REPS
+    )
+    parser.add_argument(
+        "--percent-reps",
+        type=int,
+        help="times to downsample for each percentage",
+        default=REPS_PER_PERCENT,
+    )
+    parser.add_argument(
+        "--percent-spacing",
+        type=int,
+        help="space between percents. E.g.  5 means 5, 10, 15, ...",
+        default=5,
+    )
+    parser.add_argument(
+        "--classifier",
+        choices=["knn1", "knn3", "knn5", "knn10", "lr", "svm", "rf", "ada", "mlp"],
+        required=True,
+    )
+    parser.add_argument("--dataset", choices=["diabetes", "park", "trans", "spect"], required=True)
+    parser.add_argument("--pbar", action="store_true")
+    return parser
+
+
+def classifier_from_args(args: Namespace) -> str:
+    mappings = dict(
+        knn1="KNN-1",
+        knn3="KNN-3",
+        knn5="KNN-5",
+        knn10="KNN-10",
+        lr="Logistic Regression",
+        svm="SVM Classifier",
+        rf="Random Forest",
+        ada="AdaBoosted DTree",
+        mlp="MLP",
+    )
+    return mappings[args.classifier]
+
+
+def dataset_from_args(args: Namespace) -> str:
+    mappings = dict(diabetes="Diabetes", park="Parkinsons", trans="Transfusion", spect="SPECT")
+    return mappings[args.dataset]
+
+
+def get_percent_acc_consistency(
+    model: Type,
+    model_args: Dict,
+    x: ndarray,
+    y: ndarray,
+    percent: float,
+    kfold_reps: int,
+    x_test: ndarray = None,
+    y_test: ndarray = None,
+) -> Tuple[float, float]:
+    x_down, y_down = None, None
+    if percent >= 100:
+        x_down, y_down = x, y
+    else:
+        for _ in range(100):
+            try:
+                x_down, _, y_down, _ = train_test_split(x, y, stratify=y, train_size=percent / 100)
+                break
+            except Exception:
+                pass
+    if x_down is None:
+        raise RuntimeError("Failed to downsample")
+    if (x_test is not None) and (y_test is not None):
+        errcon = ErrorConsistencyKFoldHoldout(
+            model=model,
+            x=x_down,
+            y=y_down,
+            n_splits=5,
+            model_args=model_args,
+            stratify=True,
+            empty_unions="drop",
+        )
+        results = errcon.evaluate(
+            x_test,
+            y_test,
+            repetitions=kfold_reps,
+            save_test_accs=True,
+            save_fold_accs=False,
+            parallel_reps=True,
+            loo_parallel=True,
+            turbo=True,
+            show_progress=False,
+        )
+        return np.mean(results.test_accs), np.mean(results.consistencies)
+    errcon_internal = ErrorConsistencyKFoldInternal(
+        model=model,
+        x=x_down,
+        y=y_down,
+        n_splits=5,
+        model_args=model_args,
+        stratify=True,
+        empty_unions="drop",
+    )
+    results = errcon_internal.evaluate(
+        repetitions=kfold_reps,
+        save_test_accs=True,
+        save_fold_accs=False,
+        parallel_reps=True,
+        loo_parallel=True,
+        turbo=True,
+        show_progress=False,
+    )
+    return np.mean(results.test_accs), np.mean(results.consistencies)
+
+
+# NOTE: for parkinsons and SPECT data, are going to get
+# ValueError: n_splits=5 cannot be greater than the number of members in each class.
+# for the small downsampling percents
+def holdout_downsampling_all(show_progress: bool = False) -> None:
+    disable = not show_progress
+    for dataset_name, (x, y) in DATA.items():
+        print(f"Preparing {dataset_name} data...")
+        x, x_test, y, y_test = train_test_split(x, y, test_size=0.2)
+        for classifier_name, (model, args) in CLASSIFIERS.items():
+            print(f"Testing {classifier_name} classifier on {dataset_name} data...")
+            data = np.full([N_ROWS, 3], -1, dtype=float)
+            desc_percent = "Downsampling at {}%"
+            pbar_percent = tqdm(
+                desc=desc_percent.format(COLS[0]), total=len(COLS), leave=False, disable=disable
+            )
+            row = 0
+            for percent, col in zip(PERCENTS, COLS):
+                pbar_percent.set_description(desc_percent.format(col))
+                desc_reps = "Repetition {:d}"
+                pbar_reps = tqdm(
+                    desc=desc_reps.format(0), total=REPS_PER_PERCENT, leave=False, disable=disable
+                )
+                for r in range(REPS_PER_PERCENT):
+                    pbar_reps.set_description(desc_reps.format(r))
+                    acc, cons = get_percent_acc_consistency(
+                        model, args, x, y, percent, KFOLD_REPS, x_test, y_test
+                    )
+                    data[row] = [percent, acc, cons]
+                    row += 1
+                    pbar_reps.update()
+                pbar_reps.close()
+                pbar_percent.update()
+            pbar_percent.close()
+            assert row == N_ROWS
+            df = DataFrame(
+                data=data,
+                columns=["Percent", "Accuracy", "Consistency"],
+                index=range(N_ROWS),
+                dtype=float,
+            )
+            print(df)
+            outfile = (
+                RESULTS_DIR / f"{dataset_name}_{classifier_name}__k-fold-holdout_downsample.json"
+            )
+            df.to_json(outfile)
+
+
+def holdout_downsampling(args: Namespace,) -> None:
+    disable = not args.pbar
+    dataset = dataset_from_args(args)
+    classifier = classifier_from_args(args)
+    d = float(args.percent_spacing)
+    percents = np.arange(0, 100 + d, d)[1:]
+    percents = percents[percents >= 5]
+    cols = [f"{e:1.0f}" for e in percents]
+    reps_per_percent = args.percent_reps
+    kfold_reps = args.kfold_reps
+    n_rows = len(percents) * reps_per_percent
+    outdir = args.results_dir
+    if not outdir.exists():
+        os.makedirs(outdir)
+
+    x, y = DATA[dataset]
+    print(f"Preparing {dataset} data...")
+    x, x_test, y, y_test = train_test_split(x, y, test_size=0.2)
+    model, model_args_dict = CLASSIFIERS[classifier]
+    model_args = model_args_dict[dataset]
+    print(f"Testing {classifier} classifier on {dataset} data...")
+    data = np.full([n_rows, 3], -1, dtype=float)
+    desc_percent = "Downsampling at {}%"
+    pbar_percent = tqdm(
+        desc=desc_percent.format(cols[0]), total=len(cols), leave=False, disable=disable
+    )
+    row = 0
+    for percent, col in zip(percents, cols):
+        pbar_percent.set_description(desc_percent.format(col))
+        desc_reps = "Repetition {:d}"
+        pbar_reps = tqdm(
+            desc=desc_reps.format(0), total=reps_per_percent, leave=False, disable=disable
+        )
+        for r in range(reps_per_percent):
+            pbar_reps.set_description(desc_reps.format(r))
+            acc, cons = get_percent_acc_consistency(
+                model, model_args, x, y, percent, kfold_reps, x_test, y_test
+            )
+            data[row] = [percent, acc, cons]
+            row += 1
+            pbar_reps.update()
+        pbar_reps.close()
+        pbar_percent.update()
+    pbar_percent.close()
+    print("row:", row)
+    assert row == n_rows
+    df = DataFrame(
+        data=data, columns=["Percent", "Accuracy", "Consistency"], index=range(n_rows), dtype=float
+    )
+    print(df)
+    outfile = outdir / f"{dataset}_{classifier}__k-fold-holdout_downsample.json"
+    df.to_json(outfile)
+
+
+if __name__ == "__main__":
+    parser = argparse_setup()
+    args = parser.parse_args(
+        "--classifier mlp --dataset diabetes --kfold-reps 1 --percent-reps 1 --results-dir analysis/results/testresults --pbar".split(
+            " "
+        )
+    )
+    # args = parser.parse_args()
+    filterwarnings("ignore", message="Got `batch_size`", category=UserWarning)
+    filterwarnings("ignore", message="Stochastic Optimizer")
+    holdout_downsampling(args)
+
