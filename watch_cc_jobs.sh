d228faa2e9f4bc367f57f0cf7d174243e02a8979
Derek M Berger <dmberger.dev@gmail.com>
2021-03-13 16:05:12 -0500

    tidy up watching script

--- watch_cc_jobs.sh
+++ watch_cc_jobs.sh
@@ -3,8 +3,7 @@ if [ -z "$1" ]
 then
     echo "usage: ./watch_cc_jobs NODELIST ('NODELIST' available from \`sq\` on Compute Canada)"
 else
-    sleep 180 &&
     echo "Tensorboard running at http://localhost:6008" &&
-    echo 'filter: val_acc$|train_acc|val_loss|test_acc$|lr-Adam|lr-SGD' &&
+    echo 'val_acc$|train_acc|val_loss|test_acc$|lr-Adam|lr-SGD' &&
     ssh -N -i ~/.ssh/id_siku -L localhost:6008:$1:6006 dberger@siku.ace-net.ca
 fi
