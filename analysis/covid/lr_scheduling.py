48e1d2c7f7d8248b9f6ded3bd3bdd02922f3983b
Derek M Berger <dmberger.dev@gmail.com>
2021-03-13 16:10:52 -0500

    improve plotting of LR schedules

--- analysis/covid/lr_scheduling.py
+++ analysis/covid/lr_scheduling.py
@@ -9,7 +9,7 @@ import torch
 from numpy import ndarray
 from pytorch_lightning import LightningDataModule
 from torch import Tensor
-from torch.nn import Sequential, Linear
+from torch.nn import Sequential, PReLU
 from torch.utils.data import DataLoader, TensorDataset
 from torch.utils.data.dataset import Dataset
 from pytorch_lightning.metrics.functional import accuracy, auroc, f1
@@ -68,7 +68,7 @@ def test_small_cyclic_values() -> None:
     sbn.set_style("darkgrid")
     fig, axes = plt.subplots(ncols=len(EPOCHS))
     for i, epochs in enumerate(EPOCHS):
-        model = Sequential(Linear(10, 1))
+        model = Sequential(PReLU())
         optimizer = SGD(model.parameters(), lr=0.01, weight_decay=1e-5)
         steps_per_epoch = trainloader_length(BATCH_SIZE)
         cycle_length = epochs / (1 * steps_per_epoch)
@@ -89,32 +89,27 @@ def test_small_cyclic_values() -> None:
         axes.flat[i].plot(es, lrs, color="black", label="epoch")
         axes.flat[i].set_ylabel("LR")
         axes.flat[i].set_xlabel("Epoch")
-        axes.flat[i].set_title(f"max_lr={max_lr}, base_lr={base_lr}, stepsize_up={stepsize_up}")
+        axes.flat[i].set_title(f"stepsize_up={stepsize_up}")
         axes.flat[i].legend().set_visible(True)
+    fig.suptitle(f"max_lr={max_lr}, base_lr={base_lr}, stepsize_up={stepsize_up}")
     fig.set_size_inches(w=6 * len(EPOCHS), h=4)
     plt.show()
 
 
-def test_gamma_cyclic_values() -> None:
+def test_triangular_cyclic_values() -> None:
     BATCH_SIZE = 128
     sbn.set_style("darkgrid")
     fig, axes = plt.subplots(ncols=len(EPOCHS))
     for i, epochs in enumerate(EPOCHS):
-        model = Sequential(Linear(10, 1))
+        model = Sequential(PReLU())
         optimizer = SGD(model.parameters(), lr=0.01, weight_decay=1e-5)
         steps_per_epoch = trainloader_length(BATCH_SIZE)
-        cycle_length = epochs / (1 * steps_per_epoch)
+        cycle_length = epochs / (2 * steps_per_epoch)
         stepsize_up = cycle_length // 2
         max_lr = 0.01
         base_lr = 1e-4
-        # when using gamma we need to
         scheduler = torch.optim.lr_scheduler.CyclicLR(
-            optimizer,
-            base_lr=base_lr,
-            max_lr=max_lr,
-            mode="exp_range",
-            step_size_up=stepsize_up,
-            gamma=0.999,
+            optimizer, base_lr=base_lr, max_lr=max_lr, mode="triangular", step_size_up=stepsize_up
         )
         lrs = [scheduler.get_last_lr()]
         es = [0]
@@ -127,9 +122,58 @@ def test_gamma_cyclic_values() -> None:
         axes.flat[i].plot(es, lrs, color="black", label="epoch")
         axes.flat[i].set_ylabel("LR")
         axes.flat[i].set_xlabel("Epoch")
-        axes.flat[i].set_title(f"max_lr={max_lr}, base_lr={base_lr}, stepsize_up={stepsize_up}")
+        axes.flat[i].set_title(f"stepsize_up={stepsize_up}")
+        axes.flat[i].legend().set_visible(True)
+    fig.suptitle(f"max_lr={max_lr}, base_lr={base_lr}, stepsize_up={stepsize_up}")
+    fig.set_size_inches(w=6 * len(EPOCHS), h=4)
+    plt.show()
+
+
+def test_gamma_cyclic_values() -> None:
+    BATCH_SIZE = 128
+    sbn.set_style("darkgrid")
+    fig, axes = plt.subplots(ncols=len(EPOCHS))
+    for i, epochs in enumerate(EPOCHS):
+        model = Sequential(PReLU())
+        optimizer = SGD(model.parameters(), lr=0.01, weight_decay=1e-5)
+        steps_per_epoch = trainloader_length(BATCH_SIZE)
+        cycle_length = epochs / (1 * steps_per_epoch)
+        stepsize_up = cycle_length // 2
+        max_lr = 0.01
+        base_lr = 1e-4
+        # when using gamma we need to ensure at the last epoch the max is some reasonable value
+        # IMO something like `base_lr * 4` is fine. Since the actual `cyc_max_lr` will be
+        # `max_lr * gamma ** epoch`, we can get where we want by solving this equation. That is
+        # by solving:
+        #
+        #       max_lr * gamma ** max_epochs = base_lr * 4
+        #
+        # The solution to this is:
+        #
+        #       gamma = np.exp(np.log(base_lr * 4 / max_lr)  / max_epochs)
+        scheduler = torch.optim.lr_scheduler.CyclicLR(
+            optimizer,
+            base_lr=base_lr,
+            max_lr=max_lr,
+            mode="exp_range",
+            step_size_up=stepsize_up / 2,
+            gamma=np.exp(np.log(base_lr * 10 / max_lr) / epochs),
+        )
+        lrs = [scheduler.get_last_lr()]
+        es = [0]
+        for e in range(epochs):
+            for step in range(steps_per_epoch):
+                optimizer.step()
+                scheduler.step()
+                lrs.append(scheduler.get_last_lr())
+                es.append(e)
+        axes.flat[i].plot(es, lrs, color="black", label="epoch")
+        axes.flat[i].set_ylabel("LR")
+        axes.flat[i].set_xlabel("Epoch")
+        axes.flat[i].set_title(f"stepsize_up={stepsize_up}")
         axes.flat[i].legend().set_visible(True)
     fig.set_size_inches(w=6 * len(EPOCHS), h=4)
+    fig.suptitle(f"max_lr={max_lr}, base_lr={base_lr}, stepsize_up={stepsize_up}")
     plt.show()
 
 
@@ -137,4 +181,6 @@ if __name__ == "__main__":
     # plot_decay_function(0.001)
     # plot_multiplicative_decay_function(0.01)
 
-    test_small_cyclic_values()
+    # test_small_cyclic_values()
+    test_gamma_cyclic_values()
+    # test_triangular_cyclic_values()
