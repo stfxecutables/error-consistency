48236562baa92c020a41b4193bd9bfa0ff25a499
Derek M Berger <dmberger.dev@gmail.com>
2021-03-15 19:13:58 -0400

    generate more jobs for early stopping

--- generate_aug_job_scripts.sh
+++ generate_aug_job_scripts.sh
@@ -2,21 +2,41 @@
 module load python/3.7.4
 python analysis/covid/generate_jobscripts.py --version=b1\
     --lr-schedule=cyclic --cyclic-max=1e-1 --cyclic-base=1e-4\
-    --max-epochs=3000 --batch-size=128\
-    --dropout=0.4 --weight-decay=5e-4\
+    --max-epochs=2000 --batch-size=128\
+    --dropout=0.4 --weight-decay=5e-3\
     --elastic-scale=0.3 --elastic-trans=0.3 --elastic-shear=0.2 --elastic-degree=5
 python analysis/covid/generate_jobscripts.py --version=b1 --pretrain\
     --lr-schedule=cyclic --cyclic-max=1e-1 --cyclic-base=1e-4\
-    --max-epochs=3000 --batch-size=128\
-    --dropout=0.4 --weight-decay=5e-4\
+    --max-epochs=2000 --batch-size=128\
+    --dropout=0.4 --weight-decay=5e-3\
     --elastic-scale=0.3 --elastic-trans=0.3 --elastic-shear=0.2 --elastic-degree=5
 python analysis/covid/generate_jobscripts.py --version=b1\
     --lr-schedule=cyclic --cyclic-max=1e-1 --cyclic-base=1e-4\
-    --max-epochs=3000 --batch-size=128\
-    --dropout=0.4 --weight-decay=1e-3\
+    --max-epochs=2000 --batch-size=128\
+    --dropout=0.4 --weight-decay=1e-2\
     --elastic-scale=0.3 --elastic-trans=0.3 --elastic-shear=0.2 --elastic-degree=5
 python analysis/covid/generate_jobscripts.py --version=b1 --pretrain\
     --lr-schedule=cyclic --cyclic-max=1e-1 --cyclic-base=1e-4\
-    --max-epochs=3000 --batch-size=128\
-    --dropout=0.4 --weight-decay=1e-3\
+    --max-epochs=2000 --batch-size=128\
+    --dropout=0.4 --weight-decay=1e-2\
+    --elastic-scale=0.3 --elastic-trans=0.3 --elastic-shear=0.2 --elastic-degree=5
+python analysis/covid/generate_jobscripts.py --version=b1\
+    --lr-schedule=cyclic --cyclic-max=1e-1 --cyclic-base=1e-4\
+    --max-epochs=2000 --batch-size=128\
+    --dropout=0.6 --weight-decay=5e-3\
+    --elastic-scale=0.3 --elastic-trans=0.3 --elastic-shear=0.2 --elastic-degree=5
+python analysis/covid/generate_jobscripts.py --version=b1 --pretrain\
+    --lr-schedule=cyclic --cyclic-max=1e-1 --cyclic-base=1e-4\
+    --max-epochs=2000 --batch-size=128\
+    --dropout=0.6 --weight-decay=5e-3\
+    --elastic-scale=0.3 --elastic-trans=0.3 --elastic-shear=0.2 --elastic-degree=5
+python analysis/covid/generate_jobscripts.py --version=b1\
+    --lr-schedule=cyclic --cyclic-max=1e-1 --cyclic-base=1e-4\
+    --max-epochs=2000 --batch-size=128\
+    --dropout=0.6 --weight-decay=1e-2\
+    --elastic-scale=0.3 --elastic-trans=0.3 --elastic-shear=0.2 --elastic-degree=5
+python analysis/covid/generate_jobscripts.py --version=b1 --pretrain\
+    --lr-schedule=cyclic --cyclic-max=1e-1 --cyclic-base=1e-4\
+    --max-epochs=2000 --batch-size=128\
+    --dropout=0.6 --weight-decay=1e-2\
     --elastic-scale=0.3 --elastic-trans=0.3 --elastic-shear=0.2 --elastic-degree=5
