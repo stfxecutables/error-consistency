290a535ea2755039ea9e01617253f3decc7193b4
Derek M Berger <dmberger.dev@gmail.com>
2021-03-17 10:08:16 -0400

    last test

--- generate_aug_job_scripts.sh
+++ generate_aug_job_scripts.sh
@@ -1,66 +1,65 @@
 #!/bin/bash
 module load python/3.7.4
-# no pre-training, vary learning rates
+# no pre-training, vary L2, cyclic tr2
 python analysis/covid/generate_jobscripts.py --version=b1\
-    --lr-schedule=cyclic --cyclic-max=1e-2 --cyclic-base=1e-5\
+    --lr-schedule=cyclic --cyclic-mode=tr2 --cyclic-base=1e-4 --cyclic-max=1e-2\
     --max-epochs=2000 --batch-size=128\
-    --dropout=0.6 --weight-decay=1e-2\
-    --elastic-scale=0.3 --elastic-trans=0.3 --elastic-shear=0.2 --elastic-degree=5
+    --weight-decay=1e-5\
+    --elastic-scale=0.2 --elastic-trans=0.3 --elastic-shear=0.1 --elastic-degree=5
 python analysis/covid/generate_jobscripts.py --version=b1\
-    --lr-schedule=cyclic --cyclic-max=1e-1 --cyclic-base=1e-5\
+    --lr-schedule=cyclic --cyclic-mode=tr2 --cyclic-base=1e-4 --cyclic-max=1e-2\
     --max-epochs=2000 --batch-size=128\
-    --dropout=0.6 --weight-decay=1e-2\
-    --elastic-scale=0.3 --elastic-trans=0.3 --elastic-shear=0.2 --elastic-degree=5
+    --weight-decay=1e-4\
+    --elastic-scale=0.2 --elastic-trans=0.3 --elastic-shear=0.1 --elastic-degree=5
 python analysis/covid/generate_jobscripts.py --version=b1\
-    --lr-schedule=cyclic --cyclic-max=1e-1 --cyclic-base=1e-4\
+    --lr-schedule=cyclic --cyclic-mode=tr2 --cyclic-base=1e-4 --cyclic-max=1e-2\
     --max-epochs=2000 --batch-size=128\
-    --dropout=0.6 --weight-decay=1e-2\
-    --elastic-scale=0.3 --elastic-trans=0.3 --elastic-shear=0.2 --elastic-degree=5
-# no pre-training, vary learning rates with increased weight-decay
+    --weight-decay=1e-3\
+    --elastic-scale=0.2 --elastic-trans=0.3 --elastic-shear=0.1 --elastic-degree=5
 python analysis/covid/generate_jobscripts.py --version=b1\
-    --lr-schedule=cyclic --cyclic-max=1e-2 --cyclic-base=1e-5\
+    --lr-schedule=cyclic --cyclic-mode=tr2 --cyclic-base=1e-4 --cyclic-max=1e-2\
     --max-epochs=2000 --batch-size=128\
-    --dropout=0.6 --weight-decay=5e-2\
-    --elastic-scale=0.3 --elastic-trans=0.3 --elastic-shear=0.2 --elastic-degree=5
+    --weight-decay=1e-2\
+    --elastic-scale=0.2 --elastic-trans=0.3 --elastic-shear=0.1 --elastic-degree=5
+# same with Adam
 python analysis/covid/generate_jobscripts.py --version=b1\
-    --lr-schedule=cyclic --cyclic-max=1e-1 --cyclic-base=1e-5\
+    --lr-schedule=None\
     --max-epochs=2000 --batch-size=128\
-    --dropout=0.6 --weight-decay=5e-2\
-    --elastic-scale=0.3 --elastic-trans=0.3 --elastic-shear=0.2 --elastic-degree=5
+    --weight-decay=1e-5\
+    --elastic-scale=0.2 --elastic-trans=0.3 --elastic-shear=0.1 --elastic-degree=5
 python analysis/covid/generate_jobscripts.py --version=b1\
-    --lr-schedule=cyclic --cyclic-max=1e-1 --cyclic-base=1e-4\
+    --lr-schedule=None\
     --max-epochs=2000 --batch-size=128\
-    --dropout=0.6 --weight-decay=5e-2\
-    --elastic-scale=0.3 --elastic-trans=0.3 --elastic-shear=0.2 --elastic-degree=5
-# pre-training, vary learning rates
+    --weight-decay=1e-4\
+    --elastic-scale=0.2 --elastic-trans=0.3 --elastic-shear=0.1 --elastic-degree=5
+python analysis/covid/generate_jobscripts.py --version=b1\
+    --lr-schedule=None\
+    --max-epochs=2000 --batch-size=128\
+    --weight-decay=1e-3\
+    --elastic-scale=0.2 --elastic-trans=0.3 --elastic-shear=0.1 --elastic-degree=5
+python analysis/covid/generate_jobscripts.py --version=b1\
+    --lr-schedule=None\
+    --max-epochs=2000 --batch-size=128\
+    --weight-decay=1e-2\
+    --elastic-scale=0.2 --elastic-trans=0.3 --elastic-shear=0.1 --elastic-degree=5
+# pre-training, vary L2, cyclic tr2
 python analysis/covid/generate_jobscripts.py --version=b1 --pretrain\
-    --lr-schedule=cyclic --cyclic-max=1e-2 --cyclic-base=1e-5\
+    --lr-schedule=cyclic --cyclic-mode=tr2 --cyclic-base=1e-4 --cyclic-max=1e-2\
     --max-epochs=2000 --batch-size=128\
-    --dropout=0.6 --weight-decay=1e-2\
-    --elastic-scale=0.3 --elastic-trans=0.3 --elastic-shear=0.2 --elastic-degree=5
+    --weight-decay=1e-5\
+    --elastic-scale=0.2 --elastic-trans=0.3 --elastic-shear=0.1 --elastic-degree=5
 python analysis/covid/generate_jobscripts.py --version=b1 --pretrain\
-    --lr-schedule=cyclic --cyclic-max=1e-1 --cyclic-base=1e-5\
+    --lr-schedule=cyclic --cyclic-mode=tr2 --cyclic-base=1e-4 --cyclic-max=1e-2\
     --max-epochs=2000 --batch-size=128\
-    --dropout=0.6 --weight-decay=1e-2\
-    --elastic-scale=0.3 --elastic-trans=0.3 --elastic-shear=0.2 --elastic-degree=5
+    --weight-decay=1e-4\
+    --elastic-scale=0.2 --elastic-trans=0.3 --elastic-shear=0.1 --elastic-degree=5
 python analysis/covid/generate_jobscripts.py --version=b1 --pretrain\
-    --lr-schedule=cyclic --cyclic-max=1e-1 --cyclic-base=1e-4\
+    --lr-schedule=cyclic --cyclic-mode=tr2 --cyclic-base=1e-4 --cyclic-max=1e-2\
     --max-epochs=2000 --batch-size=128\
-    --dropout=0.6 --weight-decay=1e-2\
-    --elastic-scale=0.3 --elastic-trans=0.3 --elastic-shear=0.2 --elastic-degree=5
-# pre-training, vary learning rates with increased weight-decay
+    --weight-decay=1e-3\
+    --elastic-scale=0.2 --elastic-trans=0.3 --elastic-shear=0.1 --elastic-degree=5
 python analysis/covid/generate_jobscripts.py --version=b1 --pretrain\
-    --lr-schedule=cyclic --cyclic-max=1e-2 --cyclic-base=1e-5\
+    --lr-schedule=cyclic --cyclic-mode=tr2 --cyclic-base=1e-4 --cyclic-max=1e-2\
     --max-epochs=2000 --batch-size=128\
-    --dropout=0.6 --weight-decay=5e-2\
-    --elastic-scale=0.3 --elastic-trans=0.3 --elastic-shear=0.2 --elastic-degree=5
-python analysis/covid/generate_jobscripts.py --version=b1 --pretrain\
-    --lr-schedule=cyclic --cyclic-max=1e-1 --cyclic-base=1e-5\
-    --max-epochs=2000 --batch-size=128\
-    --dropout=0.6 --weight-decay=5e-2\
-    --elastic-scale=0.3 --elastic-trans=0.3 --elastic-shear=0.2 --elastic-degree=5
-python analysis/covid/generate_jobscripts.py --version=b1 --pretrain\
-    --lr-schedule=cyclic --cyclic-max=1e-1 --cyclic-base=1e-4\
-    --max-epochs=2000 --batch-size=128\
-    --dropout=0.6 --weight-decay=5e-2\
-    --elastic-scale=0.3 --elastic-trans=0.3 --elastic-shear=0.2 --elastic-degree=5
+    --weight-decay=1e-2\
+    --elastic-scale=0.2 --elastic-trans=0.3 --elastic-shear=0.1 --elastic-degree=5
