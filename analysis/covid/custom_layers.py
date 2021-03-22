35f4af4215f3ccfb8c15268bdfc382cae7a2fafe
Derek M Berger <dmberger.dev@gmail.com>
2021-03-22 00:25:02 -0400

    refactor custom layers to file

---
+++ analysis/covid/custom_layers.py
@@ -0,0 +1,64 @@
+from typing import no_type_check
+
+import torch
+from torch import Tensor
+from torch.nn import BatchNorm2d, Conv2d, LeakyReLU, Module
+
+
+SIZE = (256, 256)
+
+
+class GlobalAveragePooling(Module):
+    """Apply the mean along a dimension.
+
+    Parameters
+    ----------
+    reduction_dim: int = 1
+        The dimension along which to apply the mean.
+    """
+
+    def __init__(self, reduction_dim: int = 1, keepdim: bool = False):
+        super().__init__()
+        self._reduction_dim = reduction_dim
+        self._keepdim = keepdim
+
+    @no_type_check
+    def forward(self, x: Tensor) -> Tensor:
+        return torch.mean(x, dim=self._reduction_dim, keepdim=self._keepdim)
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}(reducing_dim={self._reduction_dim})"
+
+    __repr__ = __str__
+
+
+class ConvUnit(Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int,
+        dilation: int = 0,
+        padding: int = 0,
+        padding_mode: str = "reflect",
+    ):
+        super().__init__()
+        self.conv = Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            dilation=dilation,
+            padding=padding,
+            padding_mode=padding_mode,
+            bias=False,
+        )
+        # self.prelu = PReLU()
+        self.prelu = LeakyReLU()
+        self.norm = BatchNorm2d(num_features=out_channels)
+
+    @no_type_check
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.conv(x)
+        x = self.prelu(x)
+        x = self.norm(x)
+        return x  # type: ignore
--- analysis/covid/efficientnet.py
+++ analysis/covid/efficientnet.py
@@ -15,9 +15,8 @@ from pytorch_lightning.callbacks import (
 )
 from pytorch_lightning.metrics.functional import accuracy
 from torch import Tensor
-from torch.nn import BatchNorm2d
 from torch.nn import BCEWithLogitsLoss as Loss
-from torch.nn import Conv2d, LeakyReLU, Module
+from torch.nn import Module
 from torch.optim import Adam
 from torch.optim.optimizer import Optimizer
 
@@ -41,62 +40,6 @@ ON_COMPUTE_CANADA = os.environ.get("CC_CLUSTER") is not None
 # note these were tested with a batch size of 128 and various regularization params:
 #
 # efficientnet-bX-pretrained/_LINEAR-TEST_lr-max=0.05@1500_L2=1.00e-05_128batch_crop+rflip+elstic
-
-
-class GlobalAveragePooling(Module):
-    """Apply the mean along a dimension.
-
-    Parameters
-    ----------
-    reduction_dim: int = 1
-        The dimension along which to apply the mean.
-    """
-
-    def __init__(self, reduction_dim: int = 0, keepdim: bool = False):
-        super().__init__()
-        self._reduction_dim = reduction_dim
-        self._keepdim = keepdim
-
-    @no_type_check
-    def forward(self, x: Tensor) -> Tensor:
-        return torch.mean(x, dim=self._reduction_dim, keepdim=self._keepdim)
-
-    def __str__(self) -> str:
-        return f"{self.__class__.__name__}(reducing_dim={self._reduction_dim})"
-
-    __repr__ = __str__
-
-
-class ConvUnit(Module):
-    def __init__(
-        self,
-        in_channels: int,
-        out_channels: int,
-        kernel_size: int,
-        dilation: int = 0,
-        padding: int = 0,
-        padding_mode: str = "reflect",
-    ):
-        super().__init__()
-        self.conv = Conv2d(
-            in_channels=in_channels,
-            out_channels=out_channels,
-            kernel_size=kernel_size,
-            dilation=dilation,
-            padding=padding,
-            padding_mode=padding_mode,
-            bias=False,
-        )
-        # self.prelu = PReLU()
-        self.prelu = LeakyReLU()
-        self.norm = BatchNorm2d(num_features=out_channels)
-
-    @no_type_check
-    def forward(self, x: Tensor) -> Tensor:
-        x = self.conv(x)
-        x = self.prelu(x)
-        x = self.norm(x)
-        return x
 
 
 class CovidEfficientNet(Module):
