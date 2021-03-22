bfe4e955421771a7b1e8220367aa50b75071790c
Derek M Berger <dmberger.dev@gmail.com>
2021-03-22 00:25:21 -0400

    improve local tensorboard.sh script

--- tensorboard.sh
+++ tensorboard.sh
@@ -1,4 +1,7 @@
+echo "http://localhost:6006/#scalars&tagFilter=train_acc%7Ctrain_loss%7Cval_acc%24%7Cval_loss%7Ctest_acc%24&regexInput=ResNet18"
+echo "train_acc|train_loss|val_acc$|val_loss|test_acc$"
 tensorboard\
     --logdir analysis/covid/logs\
     --reload_interval 30
+
 
