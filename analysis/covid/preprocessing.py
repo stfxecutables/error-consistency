00c90533518e1f9c21ddef3d2968ce23a8ca8259
Derek M Berger <dmberger.dev@gmail.com>
2021-03-21 21:35:31 -0400

    update preprocessing normalization info

--- analysis/covid/preprocessing.py
+++ analysis/covid/preprocessing.py
@@ -115,25 +115,47 @@ def convert_subset(
 
 
 def to_numpy(normalization: str = "featurewise") -> Tuple[ndarray, ...]:
+    """NOTE from PyTorch https://pytorch.org/hub/pytorch_vision_resnet/
+
+    All pre-trained models expect input images normalized in the same way, i.e.
+    mini-batches of 3-channel RGB images of shape (3 x H x W), where H and W are expected to be at
+    least 224. **The images have to be loaded in to a range of [0, 1]** and then normalized using
+    mean = [0.485, 0.456, 0.406] and std = [0.229, 0.224, 0.225].
+    """
+    resnet = normalization == "resnet"
+    x_train, y_train = convert_subset("train", resnet)
+    x_val, y_val = convert_subset("val", resnet)
+    x_test, y_test = convert_subset("test", resnet)
+    feature_means = np.mean(x_train, axis=0, keepdims=True)
+    feature_sds = np.std(x_train, axis=0, ddof=1, keepdims=True)
+    x_train -= feature_means
+    x_train /= feature_sds
+    x_val -= feature_means
+    x_val /= feature_sds
+    x_test -= feature_means
+    x_test /= feature_sds
     if normalization == "featurewise":
-        x_train, y_train = convert_subset("train")
-        x_val, y_val = convert_subset("val")
-        x_test, y_test = convert_subset("test")
-        feature_means = np.mean(x_train, axis=0, keepdims=True)
-        feature_sds = np.std(x_train, axis=0, ddof=1, keepdims=True)
-        x_train -= feature_means
-        x_train /= feature_sds
-        x_val -= feature_means
-        x_val /= feature_sds
-        x_test -= feature_means
-        x_test /= feature_sds
-    elif normalization == "resnet":
+        return x_train, y_train, x_val, y_val, x_test, y_test
+
+    # after this if we do x = np.concatenate([x_train, x_val, x_test], axis=0), we get:
+    # x.max() = 3.24, x.min() = -6.49, np.median(x) = 0.105
+    # np.round(np.percentile(x.ravel(), [1, 99]), 3) = [-2.332,  1.953]
+    # This suggests a clip to [-2.5, 2.0] is pretty lossless, and will allow sane re-scaling
+    if normalization == "resnet":
         # normalization constants from https://pytorch.org/hub/pytorch_vision_resnet/
+        MIN, MAX = -2.5, 2.0
         MEAN = np.array([0.485, 0.456, 0.406])
         STD = np.array([0.229, 0.224, 0.225])
-        x_train, y_train = convert_subset("train", resnet=True)
-        x_val, y_val = convert_subset("val", resnet=True)
-        x_test, y_test = convert_subset("test", resnet=True)
+        np.clip(x_train, MIN, MAX, out=x_train)
+        np.clip(x_val, MIN, MAX, out=x_val)
+        np.clip(x_test, MIN, MAX, out=x_test)
+        x_train = (x_train - MIN) / (MAX - MIN)
+        x_val = (x_val - MIN) / (MAX - MIN)
+        x_test = (x_test - MIN) / (MAX - MIN)
+        # Now again if we do x = np.concatenate([x_train, x_val, x_test], axis=0), we get:
+        # np.round(np.mean(x, axis=(0, 1, 2)), 3)        == [0.343, 0.343, 0.343] = mean
+        # np.round(np.std(x, axis=(0, 1, 2), ddof=1), 3) == [0.299, 0.299, 0.299] = sd
+
         # np.newaxis allows us to broadcast as we want efficiently
         # https://stackoverflow.com/questions/7140738/numpy-divide-along-axis
         x_train -= MEAN[np.newaxis, np.newaxis, :]
@@ -142,13 +164,14 @@ def to_numpy(normalization: str = "featurewise") -> Tuple[ndarray, ...]:
         x_train /= STD[np.newaxis, np.newaxis, :]
         x_val /= STD[np.newaxis, np.newaxis, :]
         x_test /= STD[np.newaxis, np.newaxis, :]
+        return x_train, y_train, x_val, y_val, x_test, y_test
     else:
         raise ValueError("`normalization` must be one of 'resnet' or 'featurewise'.")
-    return x_train, y_train, x_val, y_val, x_test, y_test
 
 
 if __name__ == "__main__":
     x_train, y_train, x_val, y_val, x_test, y_test = to_numpy("resnet")
+    # x_train, y_train, x_val, y_val, x_test, y_test = to_numpy("featurewise")
     np.save(NUMPY_DATA_ROOT / "x_train.npy", x_train)
     np.save(NUMPY_DATA_ROOT / "y_train.npy", y_train)
     np.save(NUMPY_DATA_ROOT / "x_val.npy", x_val)
