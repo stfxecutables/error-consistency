206baf428baeea596ab8224c7041f4c649a2f84b
Derek M Berger <dmberger.dev@gmail.com>
2021-05-05 21:02:42 -0400

    remove separate analyses from library

--- generate_job_scripts.sh
+++
@@ -1,5 +0,0 @@
-module load python/3.7.4
-python analysis/covid/generate_jobscripts.py --version=b1 $@
-python analysis/covid/generate_jobscripts.py --version=b1 $@
-python analysis/covid/generate_jobscripts.py --version=b1 --pretrain $@
-python analysis/covid/generate_jobscripts.py --version=b1 --pretrain $@
--- submit_all_job_scripts.sh
+++
@@ -1,1 +0,0 @@
-fd --no-ignore --regex 'submit__eff' -x sbatch {}
